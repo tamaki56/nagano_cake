193b3ed6871648f95fa8e97686722247f0b43ed2
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-14 10:27:57 +0000

    [Add]items_migrate

--- db/migrate/20220214093434_create_items.rb
+++ db/migrate/20220214093434_create_items.rb
@@ -1,6 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      t.references :genres, foreign_key: true #ジャンルid_fk
+      t.string :name, null: false
+      t.string :image_id, null: false
+      t.text :introduction, null: false
+      t.integer :price, null: false
+      t.boolean :is_active, null: false, default: true
+      t.datetime :created_at, null: false, default: now
+      t.datetime :updated_at, null: false, default: now
 
       t.timestamps
     end
