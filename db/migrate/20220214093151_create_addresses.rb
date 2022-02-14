ce8c3d483db9a86934b1e61aef261d75d65bb7b2
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-14 10:20:02 +0000

    [Add]addresses_migrate

--- db/migrate/20220214093151_create_addresses.rb
+++ db/migrate/20220214093151_create_addresses.rb
@@ -1,6 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
+      t.references :customers, foreign_key: true #会員id_fk
+      t.string :name, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.datetime :created_at, null: false, default: now
+      t.datetime :updated_at, null: false, default: now      
 
       t.timestamps
     end
