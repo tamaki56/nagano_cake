42050087641a5c555720ace2145213747daa2df2
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-14 10:50:44 +0000

    [Add]orders_details_migrate

--- db/migrate/20220214094106_create_order_details.rb
+++ db/migrate/20220214094106_create_order_details.rb
@@ -1,6 +1,13 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
+      t.references :items, foreign_key: true #商品id_fk
+      t.references :orders, foreign_key: true #注文id_fk
+      t.integer :amount, null: false
+      t.integer :price, null: false
+      t.integer :making_status, null: false, default: 0 #enum
+      t.datetime :created_at, null: false, default: now
+      t.datetime :updated_at, null: false, default: now
 
       t.timestamps
     end
