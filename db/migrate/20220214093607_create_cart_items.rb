e7f2eaf065a83bc2260a04ba45a8b6b8aa269b98
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-14 10:33:24 +0000

    [Add]cart_items_migrate

--- db/migrate/20220214093607_create_cart_items.rb
+++ db/migrate/20220214093607_create_cart_items.rb
@@ -1,6 +1,11 @@
 class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
+      t.references :items, foreign_key: true #商品id_fk
+      t.references :customers, foreign_key: true #会員id_fk
+      t.integer :amount, null: false
+      t.datetime :created_at, null: false, default: now
+      t.datetime :updated_at, null: false, default: now
 
       t.timestamps
     end
