910ca680c098421b71acacc4734adf130bcc7647
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-15 05:24:38 +0000

    [Add]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,46 @@
 Rails.application.routes.draw do
+  #管理者
   namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/show'
-    get 'customers/index'
-    get 'customers/edit'
+    resources orders,only:[:show,:update]
   end
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources customers,only:[:show,:index,:edit,:update]
   end
   namespace :admin do
-    get 'items/index'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/new'
+    resources genres,only:[:index,:edit,:create,:update]
   end
   namespace :admin do
-    get 'homes/top'
+    resources items,only:[:index,:show,:edit,:new,:create,:update]
+  end
+  namespace :admin do
+    get '/admin' => 'homes#top'
+  end
+  
+  #会員
+  namespace :public do
+    resources addresses,only:[:index,:edit,:create,:update,:destroy]
   end
   namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
+    resource customers,only:[:show,:edit,:update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
   end
   namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
+    resources cart_items,only:[:index,:update,:create]
+    delete 'cart_items/:id' => 'cart_items#destroy'
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   end
   namespace :public do
-    get 'cart_items/index'
+    resource orders,only:[:new,:index,:show,:create]
+    get 'orders/thanks' => 'orders#thanks'
+    post 'orders/confirmation' => 'orders#confirmation'
   end
   namespace :public do
-    get 'orders/new'
-    get 'orders/thanks'
-    get 'orders/index'
-    get 'orders/show'
+    root to: 'homes/top'
+    get 'homes/about' => 'homes#about'
   end
   namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
+    resources items,only:[:index,:show]
   end
 # 顧客用
 # URL /customers/sign_in ...
