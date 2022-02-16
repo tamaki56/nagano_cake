8423beb6970d43d2e75c366cc0be47f32b39f2e6
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-16 08:22:18 +0000

    [Add]admin/views

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,20 +3,21 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
     @genre = Genre.new
   end
-
-  def edit
-  end
   
   def create
     @genre = Genre.new(genre_params)
     @genre.save
     redirect_to admin_genres_path
-    
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
   end
   
   def update
-  
-    
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
   
   private
