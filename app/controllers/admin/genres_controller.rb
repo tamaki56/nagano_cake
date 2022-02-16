c9d41936db85444f4561788ada61447d5de8eace
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-16 06:13:55 +0000

    [Add]admin/viwes

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,11 +1,16 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.all
+    @genre = Genre.new
   end
 
   def edit
   end
   
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
     
   end
   
@@ -13,4 +18,11 @@ class Admin::GenresController < ApplicationController
   
     
   end
+  
+  private
+    
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,16 +14,14 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
-    @item = Item.new(item_prams)
+    @item = Item.new(item_params)
     @item.save
     redirect_to admin_item_path(@item.id)
   end
   
   def update
-  
-    
   end
   
   private
