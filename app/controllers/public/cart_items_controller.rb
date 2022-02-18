6db625c3416df9cfaf085c5587c8d65fb0bbf11f
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-18 01:19:39 +0000

    [Add]public/views

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+    
   end
   
   def update
@@ -13,6 +14,14 @@ class Public::CartItemsController < ApplicationController
   end
   
   def destroy_all
-    
+    @cart_item = Cart_item.find(params[:id])
+    @cart_item.destroy.all
+    redirect_to cart_items_path
   end
+  
+  private
+    
+  def cart_item_params
+    params.require(:cart_item).permit(:name, :price, :amount)
+  end  
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,5 +3,13 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
+  
+  private
+    
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id, :amount)
+  end  
 end
