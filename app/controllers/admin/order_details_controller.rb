55d8f31bd4ff263d7b0fff078d7ebd05c052dea2
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-23 10:31:25 +0000

    [Add]admin/order.statse

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    #ステータス更新の連動
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "production"
+      @order = @order_detail.order
+      @order.update(status: 2)
+    elsif @order_detail.making_status == "production_completed"
+        @order = @order_detail.order
+        @order.update(status: 3)
+    end
+      redirect_to admin_order_path(@order.id)
   end 
+  
+  private
+    
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :making_status)
+  end  
 end
